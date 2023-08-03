ed0a2ffb4be368a406505d760daec480291abcf4
miloupark <helenapark96@naver.com>
2023-08-03 12:07:01 +0900

    just쇼핑몰-slider-script

---
+++ 05-craftsmanWebDesign/01-just쇼핑몰/js/comm.js
@@ -0,0 +1,37 @@
+$(function () {
+
+    // modal
+    $("#modal").removeClass("active");
+    // 이벤트 시작 시 modal을 없애고 홈페이지 시작!
+
+    // modal 열기
+    // .lst-bbs li:first-child 를 클릭했을 때
+    $(".lst-bbs li:first-child").on("click", function() {
+        // #modal에 .active를 추가
+        $("#modal").addClass("active");
+        // addClass도 카멜표기법
+    });
+    
+    // modal 닫기
+    // #modal button 을 클릭했을 때 
+    $("#modal button").on("click", function() {
+        // #modal에 .active를 삭제
+        $("#modal").removeClass("active");
+    });
+
+    // tabs
+    // .tab-item 옆에 .on 삭제
+    $(".tab-item").removeClass("on");
+    // .tab-item:first-child 에게 .on 추가
+    $(".tab-item:first-child").addClass("on");
+
+    // .tab-item .tit 을 클릭했을 때
+    $(".tab-item .tit").on("click", function() {
+        // .tab-item에 붙어 있는 .on 삭제
+        $("tab-item").removeClass("on");
+
+        // 방금 클릭한 나(this)의 부모(parent())에게 .on 추가
+        $(this).parent().addClass("on");
+    });
+});
+ 
