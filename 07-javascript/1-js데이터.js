b359d4865eaa551a98ab131838b6c74619eead46
miloupark <helenapark96@naver.com>
2023-09-11 11:41:12 +0900

    javascript

---
+++ 07-javascript/1-js데이터.js
@@ -0,0 +1,13 @@
+// string (문자열)
+
+const string1 = 'hello'; // 큰 따옴표 가능 
+const string2 = "hi"; // 작은 따옴표 가능 
+const string3 = `hihihi ${string1} ${1+1}`; // 백틱 가능 
+
+console.log(string1, string2, string3);
+
+// number (숫자)
+const number = 123;
+// 123;에 따옴표를 넣으면 문자 그대로 출력됨
+
+console.log(number);
