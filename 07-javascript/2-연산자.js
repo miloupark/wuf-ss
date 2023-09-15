35ab8483adf2abeb83bf64a6f73699cf1b0b35e1
miloupark <helenapark96@naver.com>
2023-09-15 13:20:03 +0900

    js

--- 07-javascript/2-연산자.js
+++ 07-javascript/2-연산자.js
@@ -21,9 +21,10 @@ console.log(isEven(3)); /* true, false로 나타남 */
 console.log(isEven(4));
 
 
-// 할당 연산사
+// 할당 연산자
 let a = 2;
 console.log(a); /* 2 */
+// const는 재할당 불가
 
 // 재할당
 a = 3;
@@ -67,3 +68,127 @@ console.log(![]); /* false */
 console.log(!{}); /* false */
 console.log(!null); /* true */
 console.log(!function(){}); /* true */
+
+
+// 비교연산자 
+// ==, ===, !=, !==, >, >=, <, <=
+console.log(1 == '1'); /* 동등연산자 */
+console.log(1 === '1'); /* 일치연산자 */
+console.log(1 != '1'); /* false (부동연산자) */
+console.log(1 !== '1'); /* true (불일치연산자) */
+console.log(1 > 2); /* false */
+console.log(1 < 2); /* true */
+console.log(1 >= 2); /* false */
+console.log(1 <= 2); /* true */
+
+
+// 논리연산자
+// &&, ||
+
+const f = true;
+const g = true;
+
+if(f && g) {
+  console.log('둘 다 참입니다.');
+}
+// 둘 다 참일 경우에만 출력됨 
+console.log(true && false); /* false */
+console.log(1 && 0); /* 0 */
+console.log(1 && 2 && 0); /* 0 */
+console.log(1 && 0 && 3); /* 0 */
+console.log('a' & 'b' && ''); /* '' */ 
+console.log('a' & 'b' && 'c') /* c */
+
+
+// 또 (||)는 연산자
+const h = false;
+const i = true;
+
+if (h || i) {
+  console.log('둘 중 하나라도 참입니다.');
+}
+
+
+// 연산자를 기준으로 가장 먼저 만나는 참 데이터가 반환된다.
+console.log(true || false); /* true */
+console.log(1 || 0); /* 1 */
+console.log(false || {} || 0); /* {} 객체데이터 반환(참) */
+console.log(false || [] || null); /* [] 배열 반환(참) */
+console.log(function () {} || '' || undefined); /* [] 배열 반환(참) */
+console.log(false || 0 || '' || NaN); /* NaN / 모두가 거짓이라면 마지막 데이터가 반환 */
+
+
+// 병합 연산자
+// a ?? b
+const j = 0;
+console.log(j || 5); /* 5 */
+console.log(j ?? 5); /* 0 */
+console.log(null ?? 1); /* 1 */
+console.log(undefined ?? 1); /* 1 */
+console.log(undefined ?? null); /* null */
+console.log(null ?? 1 ?? 2); /* 1 */
+console.log(0 ?? 1 ?? 2); /* 0 */
+
+
+// 삼항 연산자
+// 조건? 참 : 거짓 
+console.log(true ? 1 : 2); /* 1 */
+console.log(false ? 1 : 2); /* 2 */
+
+
+// if문
+let p = 1; /* p = 5로 변경하면 큽니다로 바뀜 */ 
+
+// if (p < 3) {
+//   console.log('p는 3보다 작습니다.');
+// }else {
+//   console.log('p는 3보다 큽니다.');
+// }
+
+console.log(p < 3 ? '참' : '거짓'); /* 참 */
+
+// ex
+function isAnimal(text) {
+  return text === '고양이' ? '고양이' : '고양이 아님'
+}
+console.log(isAnimal('고양이')); /* 고양이 */
+console.log(isAnimal('개')); /* 고양이 아님 */
+
+
+// 전개연산자
+// 배열의 전개
+const q = [1, 2, 3];
+const r = [4, 5, 6];
+
+console.log(q.concat(r)); /* [1, 2, 3, 4, 5, 6] */
+console.log([...q,...r]); /* [1, 2, 3, 4, 5, 6] */
+
+
+// 객체의 전개
+const userC = {
+  name: '김부모',
+  age: 50,
+};
+
+const userD = {
+  ...userC,
+  nick: '김자식',
+  // parent: userC,
+}
+console.log(userC);
+console.log(userD);
+
+
+// 함수의 인자에서의 전개
+function sum(a, b, c) {
+  console.log(a + b + c)
+}
+
+sum(1, 2, 3) /* 6 */
+
+// 배열 데이터를 인자로 전달하려면?
+const num = [1, 2, 3]
+sum(num[0], num[1], num[2]) // 6
+
+// 전개 연산자를 사용하면?
+sum(...num) // 6
