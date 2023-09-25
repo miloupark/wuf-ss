c3e30588d77d29bc436ef122e86a06eaeb6d1eb5
miloupark <helenapark96@naver.com>
2023-09-25 13:20:24 +0900

    함수

--- 07-javascript/4-함수.js
+++ 07-javascript/4-함수.js
@@ -40,3 +40,90 @@ function hello4() {
 
 
 console.log(hello1(), hello2(), hello3(), hello4())
+
+
+// 예시
+function getGrade(score) {
+  if (score === 100) {
+    // A+
+    return 'A+'
+  } else if (score >= 90) {
+    // A
+    return 'A'
+  } else if (score >= 80) {
+    // B
+    return 'B'
+  } else if (score >= 70) {
+    // C
+    return 'C'
+  } else if (score >= 60) {
+    // D
+    return 'D'
+  } else { 
+    // F  
+    return 'F'
+  }
+}
+
+console.log(getGrade(50))
+
+
+// 매개변수 패턴
+function sum(x, y = 1) {
+  return x + y
+}
+
+console.log(sum(1, 5))
+console.log(sum(1)) /* NaN = Not a Number */
+
+
+// 객체의 구조 분해 할당
+/* {} = 객체 */
+const user = {
+  name : 'winter',
+  age : 2,
+  email : 'helenapark96@naver.com'
+} 
+
+function getName ({ name }) {
+  return name
+}
+function getEmail ({ email = '이메일이 없습니다.'}) {
+  return email
+}
+
+console.log(getName(user))
+console.log(getEmail(user))
+
+
+// 배열 구조 분해 할당
+const animals = ['dog', 'cat', 'fish'];
+
+// 두번째 요소 반환하는 기능
+function getSecond([, b, c]) {
+  return [b, c]
+}
+
+console.log(getSecond(animals));
+
+
+// 화살표 함수
+function 함수이름(매개변수) {실행문}
+
+(매개변수) => {실행문}
+
+function hello() {
+  console.log('hello')
+}
+
+const hello11 = function () {
+  console.log('hello11')
+}
+
+const hello12 = () => {
+  console.log('hello12')
+}
+
+hello()
+hello11()
+hello12()
